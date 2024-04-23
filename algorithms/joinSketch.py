fbb3f8c93dfe7af118311307fcb03ece525d3751
stellaHolbrook <jh8839@nyu.edu>
2024-04-23 19:01:26 -0400

    count sketch, join sketch

--- algorithms/joinSketch.py
+++ algorithms/joinSketch.py
@@ -4,4 +4,24 @@ from algorithms.algorithm_adaptor import algorithm_adaptor
 import random
 import hashlib
 import mmh3 as mmh
+import ctypes
+import numopy as np
+def SIMD_match_8(id, key):
+    # f this im not doing SIMD instructions in python
+    return None
+
+class hashTable:
+    def __init__(self, bucketSum, hashSeed):
+        self.n = bucketSum
+        self.id = np.empty(self.n)
+        self.m = 8
+        for i in range(len(n)):
+            self.id[i] = np.empty([self.m], dtype=int)
+        key =  np.empty(self.n)
+        for i in range(len(n)):
+            key[i] = np.empty([self.m], dtype=int)
+        for i in range(len(n)):
+            for j in range(len(m)):
+                self.id[i][j] = key[i][j] = 0
+        # should just be done with np.zeroes???
 
